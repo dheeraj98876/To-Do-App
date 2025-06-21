215922894de79491683ec5baa317b48b9ddf16a2
D S Bisht <dheerajbisht2415@gmail.com>
2025-06-21 21:30:10 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
 
  <StrictMode>
-  <header className='header'><h1> To Do App</h1> </header>
     <App />
-  <footer className='footer'> Copyright © 2025 Dheeraj Singh Bisht To Do App  </footer>
   </StrictMode>,
 )
